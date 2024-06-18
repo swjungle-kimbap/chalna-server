f450dc3733e432020b4f158313679a190f221a84
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-19 01:15:11 +0900

    feat: 전역 예외 상태정보 생성 (#2)

---
+++ src/main/java/com/jungle/chalnaServer/global/exception/GlobalErrorCode.java
@@ -0,0 +1,26 @@
+package com.jungle.chalnaServer.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.util.Set;
+
+@Getter
+public enum GlobalErrorCode {
+    UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"알 수 없는 오류가 발생했습니다.",Set.of()),
+    RUNTIME_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"서버 작업중 오류가 발생했습니다.",Set.of(GlobalException.class));
+
+    private final HttpStatusCode status;
+    private final String code;
+    private final String message;
+    private final Set<Class<? extends Exception>> exceptions;
+
+    GlobalErrorCode(HttpStatusCode status, String message, Set<Class<? extends Exception>> exceptions) {
+        this.status = status;
+        this.code = String.valueOf(status.value());
+        this.message = message;
+        this.exceptions = exceptions;
+    }
+}
