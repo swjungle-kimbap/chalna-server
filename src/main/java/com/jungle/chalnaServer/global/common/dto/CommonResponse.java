7bc310d61b8f47f00e12db05ac8bfcea575ce721
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-19 01:45:04 +0900

    feat: CommonResponse 정적팩토리 메소드 추가

--- src/main/java/com/jungle/chalnaServer/global/common/dto/CommonResponse.java
+++ src/main/java/com/jungle/chalnaServer/global/common/dto/CommonResponse.java
@@ -1,8 +1,15 @@
 package com.jungle.chalnaServer.global.common.dto;
 
 import com.jungle.chalnaServer.global.exception.GlobalErrorCode;
+import org.springframework.http.HttpStatus;
 
 public record CommonResponse<T>(String code, T data, String message) {
+    public static CommonResponse<?> ok(Object data, String message){
+        return new CommonResponse<>(String.valueOf(HttpStatus.OK.value()),data,message);
+    }
+    public static CommonResponse<?> from(HttpStatus httpStatus, Object data, String message){
+        return new CommonResponse<>(String.valueOf(httpStatus.value()),data,message);
+    }
     public static CommonResponse<?> from(String code,Object data, String message){
         return new CommonResponse<>(code,data,message);
     }
