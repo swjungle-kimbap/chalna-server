43b1d7307ac662310624d38c09647f98784f26c8
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-18 23:52:57 +0900

    chore : project init

---
+++ src/main/java/com/jungle/chalnaServer/ChalnaServerApplication.java
@@ -0,0 +1,13 @@
+package com.jungle.chalnaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalnaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChalnaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jungle/chalnaServer/ChalnaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungle.chalnaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalnaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
