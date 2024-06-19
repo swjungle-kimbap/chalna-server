641e4803d1362bdb06e5760e1b5cc5230d4e487b
jujulia3040@gmail.com <jujulia3040@gmail.com>
2024-06-20 02:27:41 +0900

    Feat: FCM 에러 코드 정의 (#6)

--- src/main/java/com/jungle/chalnaServer/global/exception/GlobalErrorCode.java
+++ src/main/java/com/jungle/chalnaServer/global/exception/GlobalErrorCode.java
@@ -1,18 +1,18 @@
 package com.jungle.chalnaServer.global.exception;
 
 import com.jungle.chalnaServer.domain.member.exception.MemberNotFoundException;
+import com.jungle.chalnaServer.infra.fcm.exception.FCMTokenNotFoundException;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.Set;
 
 @Getter
 public enum GlobalErrorCode {
     UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 오류가 발생했습니다.", Set.of()),
-    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST,"해당 회원을 찾을 수 없습니다.",Set.of(MemberNotFoundException.class));
-
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST,"해당 회원을 찾을 수 없습니다.",Set.of(MemberNotFoundException.class)),
+    FCM_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 회원의 토큰을 찾을 수 없습니다.", Set.of(FCMTokenNotFoundException.class));
 
     private final HttpStatusCode status;
     private final String code;
