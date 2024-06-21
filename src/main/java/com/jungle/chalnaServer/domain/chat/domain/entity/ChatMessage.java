91cd7aa853d109e0aa4223f5af94b81360353b28
Dain Lee <dlekdls529@gmail.com>
2024-06-21 15:13:35 +0900

    refactor: Date Format 수정

--- src/main/java/com/jungle/chalnaServer/domain/chat/domain/entity/ChatMessage.java
+++ src/main/java/com/jungle/chalnaServer/domain/chat/domain/entity/ChatMessage.java
@@ -1,19 +1,19 @@
 package com.jungle.chalnaServer.domain.chat.domain.entity;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
-@Builder
+//@Builder
 @Getter
-public class ChatMessage{
+@Setter
+@NoArgsConstructor
+public class ChatMessage {
     private Long id;
     private MessageType type;
     private Integer senderId;
@@ -21,15 +21,40 @@ public class ChatMessage{
     private String content;
     private Boolean status; // 읽음 여부(True: 안읽음, False: 읽음)
 
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+//    @JsonSerialize(using = LocalDateTimeSerializer.class)
+//    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @CreatedDate
     private LocalDateTime createdAt;
 
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+//    @JsonSerialize(using = LocalDateTimeSerializer.class)
+//    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    public ChatMessage(Long id, MessageType type, Integer senderId, Integer chatRoomId, String content, Boolean status,LocalDateTime createdAt, LocalDateTime updatedAt){
+        this.id = id;
+        this.type = type;
+        this.senderId = senderId;
+        this.chatRoomId = chatRoomId;
+        this.content = content;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+//    @JsonCreator
+//    public ChatMessage(@JsonProperty("id") Long id, @JsonProperty("type") MessageType type,
+//                       @JsonProperty("senderId") Integer senderId, @JsonProperty("chatRoomId") Integer chatRoomId,
+//                       @JsonProperty("content") String content, @JsonProperty("status") Boolean status){
+//        this.id = id;
+//        this.type = type;
+//        this.senderId = senderId;
+//        this.chatRoomId = chatRoomId;
+//        this.content = content;
+//        this.status = status;
+//    }
 
     public enum MessageType {
         CHAT, // 그냥 메시지
