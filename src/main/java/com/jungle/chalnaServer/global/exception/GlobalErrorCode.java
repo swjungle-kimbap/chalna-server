6bd5d78e6d95512ebb94495b22ea567e9075567b
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-21 20:38:07 +0900

    feat: Relation 동일 사용자 생성,조회 예외 클래스 등록 (#30)

--- src/main/java/com/jungle/chalnaServer/global/exception/GlobalErrorCode.java
+++ src/main/java/com/jungle/chalnaServer/global/exception/GlobalErrorCode.java
@@ -2,6 +2,7 @@ package com.jungle.chalnaServer.global.exception;
 
 import com.jungle.chalnaServer.domain.member.exception.FileStorageException;
 import com.jungle.chalnaServer.domain.member.exception.MemberNotFoundException;
+import com.jungle.chalnaServer.domain.relation.exception.RelationIdInvalidException;
 import com.jungle.chalnaServer.global.auth.jwt.exception.InvalidJwtTokenException;
 import com.jungle.chalnaServer.infra.fcm.exception.FCMTokenNotFoundException;
 import lombok.Getter;
@@ -19,7 +20,8 @@ public enum GlobalErrorCode {
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST,"올바르지 않은 입력 값입니다.",Set.of(HttpMessageNotReadableException.class,MethodArgumentNotValidException.class)),
     INVALID_TOKEN(HttpStatus.FORBIDDEN,"유효하지 않은 토큰입니다.",Set.of(InvalidJwtTokenException.class)),
     FCM_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 회원의 토큰을 찾을 수 없습니다.", Set.of(FCMTokenNotFoundException.class)),
-    FILE_STORAGE(HttpStatus.BAD_REQUEST,"저장할 디렉토리를 찾을 수 없습니다.",Set.of(FileStorageException.class));
+    FILE_STORAGE(HttpStatus.BAD_REQUEST,"저장할 디렉토리를 찾을 수 없습니다.",Set.of(FileStorageException.class)),
+    RELATION_ID_INVALID(HttpStatus.BAD_REQUEST,"유효하지 않은 관계입니다.",Set.of(RelationIdInvalidException.class));
 
     private final HttpStatusCode status;
     private final String code;
