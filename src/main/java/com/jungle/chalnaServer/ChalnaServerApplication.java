3b1e25a7dc7e1a22f252c25e38bea6c45496f0ae
Dain Lee <dlekdls529@gmail.com>
2024-06-21 15:12:40 +0900

    feat: EnableJpaAuditing 추가

--- src/main/java/com/jungle/chalnaServer/ChalnaServerApplication.java
+++ src/main/java/com/jungle/chalnaServer/ChalnaServerApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ChalnaServerApplication {
 
     public static void main(String[] args) {
