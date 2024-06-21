8fb465d7edbcd6f09e443d7f809669eef4cc4366
Dain Lee <dlekdls529@gmail.com>
2024-06-21 15:20:10 +0900

    refactor: conflict 수정

--- src/main/java/com/jungle/chalnaServer/ChalnaServerApplication.java
+++ src/main/java/com/jungle/chalnaServer/ChalnaServerApplication.java
@@ -3,8 +3,6 @@ package com.jungle.chalnaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
