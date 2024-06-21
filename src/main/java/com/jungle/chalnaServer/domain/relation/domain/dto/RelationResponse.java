88a7af6315a349e756466cf00a5d338436e9b186
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-21 16:20:30 +0900

    feat: relation dto 구성 (#30)

---
+++ src/main/java/com/jungle/chalnaServer/domain/relation/domain/dto/RelationResponse.java
@@ -0,0 +1,16 @@
+package com.jungle.chalnaServer.domain.relation.domain.dto;
+
+import com.jungle.chalnaServer.domain.relation.domain.entity.FriendStatus;
+import com.jungle.chalnaServer.domain.relation.domain.entity.Relation;
+
+public record RelationResponse(Long id,Long otherId,FriendStatus friendStatus, boolean isBlocked, Integer overlapCount) {
+    public RelationResponse of(Relation relation){
+        return new RelationResponse(
+        relation.getRelationPK().getId()
+                ,relation.getRelationPK().getOtherId()
+                ,relation.getFriendStatus()
+                ,relation.isBlocked()
+                ,relation.getOverlapCount()
+        );
+    }
+}
