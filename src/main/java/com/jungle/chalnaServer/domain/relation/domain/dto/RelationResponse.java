959ed66f310d703d43f88f58a6efd01bdb4a39d4
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-21 20:37:49 +0900

    feat: Relation Response 수정 (#30)

--- src/main/java/com/jungle/chalnaServer/domain/relation/domain/dto/RelationResponse.java
+++ src/main/java/com/jungle/chalnaServer/domain/relation/domain/dto/RelationResponse.java
@@ -3,14 +3,15 @@ package com.jungle.chalnaServer.domain.relation.domain.dto;
 import com.jungle.chalnaServer.domain.relation.domain.entity.FriendStatus;
 import com.jungle.chalnaServer.domain.relation.domain.entity.Relation;
 
-public record RelationResponse(Long id,Long otherId,FriendStatus friendStatus, boolean isBlocked, Integer overlapCount) {
-    public RelationResponse of(Relation relation){
+public record RelationResponse(Long id, Long otherId, FriendStatus friendStatus, boolean isBlocked,
+                               Integer overlapCount) {
+    public static RelationResponse of(Relation relation) {
         return new RelationResponse(
-        relation.getRelationPK().getId()
-                ,relation.getRelationPK().getOtherId()
-                ,relation.getFriendStatus()
-                ,relation.isBlocked()
-                ,relation.getOverlapCount()
+                relation.getRelationPK().getId()
+                , relation.getRelationPK().getOtherId()
+                , relation.getFriendStatus()
+                , relation.isBlocked()
+                , relation.getOverlapCount()
         );
     }
 }
