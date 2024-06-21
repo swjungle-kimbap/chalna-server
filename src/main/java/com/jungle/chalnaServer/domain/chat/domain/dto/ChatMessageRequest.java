0341ebaac97a79bbb5ff079f33738d8e5b7f0d10
Dain Lee <dlekdls529@gmail.com>
2024-06-21 15:13:51 +0900

    feat: NoArgsConstructor 추가

--- src/main/java/com/jungle/chalnaServer/domain/chat/domain/dto/ChatMessageRequest.java
+++ src/main/java/com/jungle/chalnaServer/domain/chat/domain/dto/ChatMessageRequest.java
@@ -2,8 +2,10 @@ package com.jungle.chalnaServer.domain.chat.domain.dto;
 
 import com.jungle.chalnaServer.domain.chat.domain.entity.ChatMessage;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ChatMessageRequest {
     ChatMessage.MessageType type;
     String content;
