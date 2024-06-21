122b765a08e6cb932600a66d22bc7f8c8248513c
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-21 20:19:55 +0900

    feat: Relation Repository 구현 (#30)

---
+++ src/main/java/com/jungle/chalnaServer/domain/relation/repository/RelationRepository.java
@@ -0,0 +1,15 @@
+package com.jungle.chalnaServer.domain.relation.repository;
+
+import com.jungle.chalnaServer.domain.relation.domain.entity.Relation;
+import com.jungle.chalnaServer.domain.relation.domain.entity.RelationPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RelationRepository extends JpaRepository<Relation, RelationPK> {
+    @Query("SELECT r from Relation r WHERE r.relationPK.id = :id")
+    List<Relation> findById(@Param("id") Long id);
+
+}
