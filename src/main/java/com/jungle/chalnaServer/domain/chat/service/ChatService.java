2c4ecd623d37b5420bbd73ad5f7ffef777d59d2d
Dain Lee <dlekdls529@gmail.com>
2024-06-21 15:15:09 +0900

    feat: sendMessage status, type 추가

--- src/main/java/com/jungle/chalnaServer/domain/chat/service/ChatService.java
+++ src/main/java/com/jungle/chalnaServer/domain/chat/service/ChatService.java
@@ -25,20 +25,19 @@ public class ChatService {
     @Autowired
     private WebSocketEventListener webSocketEventListener;
 
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
-
 
     public void sendMessage(String roomId, ChatMessageRequest requestMessage) {
         Long id = chatRepository.makeMessageId();
         Integer senderId = 1; // 임시 사용자 ID, 추후 사용자 정보 가져와서 수정해야 함.
         LocalDateTime now = LocalDateTime.now();
-
+        Boolean status = true;
         // push 알림 보내기. 채팅룸에 멤버 정보를 확인해서 다른 멤버가 채팅방에 없는 경우 알림 보내기
         if (webSocketEventListener.getConnectedCount(roomId) == 1){
             log.info("send push message");
+            status = false;
             // push 메시지 보내기
         }
+
 
         // 메시지 전달
         ChatMessageResponse responseMessage = ChatMessageResponse.builder()
@@ -47,26 +46,16 @@ public class ChatService {
                 .type(requestMessage.getType())
                 .senderId(senderId)
                 .createdAt(now)
+                .status(status)
                 .build();
 
-        log.info("responseMessage: " + responseMessage);
         messagingTemplate.convertAndSend("/topic/" + roomId, responseMessage);
 
-
         // Redis에 메시지 저장
-        ChatMessage message = ChatMessage.builder()
-                .id(id)
-                .chatRoomId(Integer.parseInt(roomId))
-                .content(requestMessage.getContent())
-                .senderId(senderId)
-                .type(requestMessage.getType())
-                .status(false) // 읽음으로 표시
-                .createdAt(now)
-                .updatedAt(now)
-                .build();
+        ChatMessage message = new ChatMessage(id, requestMessage.getType(), senderId,
+                Integer.parseInt(roomId), requestMessage.getContent(),  status,
+                now, now);
 
-        log.info("-------------");
-        log.info(message);
         chatRepository.saveMessage(message);
     }
 }
