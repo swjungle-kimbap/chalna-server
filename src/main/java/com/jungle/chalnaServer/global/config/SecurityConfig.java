1b56f237d4c73efbc98194ab3120eab171afbff5
Dain Lee <dlekdls529@gmail.com>
2024-06-23 02:09:43 +0900

    hotfix: 테스트 rollback

--- src/main/java/com/jungle/chalnaServer/global/config/SecurityConfig.java
+++ src/main/java/com/jungle/chalnaServer/global/config/SecurityConfig.java
@@ -16,9 +16,6 @@ import org.springframework.security.config.annotation.web.configurers.CorsConfig
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -54,17 +51,5 @@ public class SecurityConfig {
             exceptionHandle.authenticationEntryPoint(jwtAuthDeniedHandler);
         });
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); // 허용할 출처 설정
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/ws/**", configuration);
-        return source;
     }
 }
