bc588d0847dc6c976d04823be9a3f778968a4388
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-22 19:28:42 +0900

    chore: Member,Relation Repository 설정 추가 (#39)

--- src/main/java/com/jungle/chalnaServer/domain/relation/repository/RelationRepository.java
+++ src/main/java/com/jungle/chalnaServer/domain/relation/repository/RelationRepository.java
@@ -4,11 +4,12 @@ import com.jungle.chalnaServer.domain.relation.domain.entity.Relation;
 import com.jungle.chalnaServer.domain.relation.domain.entity.RelationPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface RelationRepository extends JpaRepository<Relation, RelationPK> {
+public interface RelationRepository extends JpaRepository<Relation, RelationPK>, QuerydslPredicateExecutor<Relation> {
     @Query("SELECT r from Relation r WHERE r.relationPK.id = :id")
     List<Relation> findById(@Param("id") Long id);
 
