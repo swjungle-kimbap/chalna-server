635f455aeb00a4592cce27fcf0b07de7aeb75739
kju0 <jujulia3040@gmail.com>
2024-07-01 13:39:44 +0900

    refactor: 이전으로 다시 복귀,,  (#156)

--- src/main/java/com/jungle/chalnaServer/global/config/FCMConfig.java
+++ src/main/java/com/jungle/chalnaServer/global/config/FCMConfig.java
@@ -2,12 +2,10 @@ package com.jungle.chalnaServer.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -17,31 +15,19 @@ import com.google.firebase.messaging.FirebaseMessaging;
 @Configuration
 public class FCMConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(FCMConfig.class);
-
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        // 환경 변수에서 서비스 계정 키 경로 가져오기
-        String googleCredentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+        InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
 
-        // 경로가 null 또는 비어있는지 확인
-        if (googleCredentialsPath == null || googleCredentialsPath.trim().isEmpty()) {
-            logger.error("Google application credentials path is not provided");
-            throw new IOException("Google application credentials path is not provided");
+        if (serviceAccount == null) {
+            throw new IOException("File not found: serviceAccountKey.json");
         }
 
-        logger.info("Initializing Firebase with provided service account key from environment variable");
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
 
-        // 파일 입력 스트림을 통해 서비스 계정 키 로드
-        try (InputStream serviceAccount = new FileInputStream(googleCredentialsPath)) {
-            // Firebase 옵션 설정
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            // Firebase 초기화
-            return FirebaseApp.initializeApp(options);
-        }
+        return FirebaseApp.initializeApp(options);
     }
 
     @Bean
