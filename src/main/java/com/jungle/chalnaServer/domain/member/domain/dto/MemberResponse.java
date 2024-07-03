e9926cd02d8d3c5af552164cfb1da009c70ea4d1
leedabin2 <12193152@inha.edu>
2024-07-03 18:01:42 +0900

    feat: (#187) 사용자 프로필, 상태메시지, 닉네임 api 분리및 s3 버킷에 저장하도록 수정

--- src/main/java/com/jungle/chalnaServer/domain/member/domain/dto/MemberResponse.java
+++ src/main/java/com/jungle/chalnaServer/domain/member/domain/dto/MemberResponse.java
@@ -2,8 +2,15 @@ package com.jungle.chalnaServer.domain.member.domain.dto;
 
 import com.jungle.chalnaServer.domain.member.domain.entity.Member;
 
-public record MemberResponse(Long id, String username, String message, String profileImageUrl) {
+public record MemberResponse(Long id, String username, String message) {
     public static MemberResponse of(Member member){
-        return new MemberResponse(member.getId(), member.getUsername(), member.getMessage(), member.getProfileImageUrl());
+        return new MemberResponse(member.getId(), member.getUsername(), member.getMessage());
     }
+
+    public record UPLOAD(Long fileId, String presignedUrl) {
+        public static MemberResponse.UPLOAD of(Long fileId, String presignedUrl) {
+            return new MemberResponse.UPLOAD(fileId,presignedUrl);
+        }
+    }
+
 }
