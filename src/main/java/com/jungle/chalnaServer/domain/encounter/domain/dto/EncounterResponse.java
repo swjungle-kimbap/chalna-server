eda09a2921b9d954c354c88945a2eea96201c2c7
leedabin2 <12193152@inha.edu>
2024-07-14 14:22:10 +0900

    fix: encounter response 만난 시간 보내주도록 수정

--- src/main/java/com/jungle/chalnaServer/domain/encounter/domain/dto/EncounterResponse.java
+++ src/main/java/com/jungle/chalnaServer/domain/encounter/domain/dto/EncounterResponse.java
@@ -1,16 +1,19 @@
 package com.jungle.chalnaServer.domain.encounter.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jungle.chalnaServer.domain.encounter.domain.entity.Encounter;
 
+import java.time.LocalDateTime;
+
 public class EncounterResponse {
-    public record LOCATION(Long memberId, Long otherId,Double latitude, Double longitude) {}
+
+    public record LOCATION(Double latitude, Double longitude,  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime meetTime) {}
 
     public static EncounterResponse.LOCATION of(Encounter encounter) {
         return new EncounterResponse.LOCATION(
-                encounter.getMemberId(),
-                encounter.getOtherId(),
                 encounter.getLatitude(),
-                encounter.getLongitude()
+                encounter.getLongitude(),
+                encounter.getCreatedAt()
         );
     }
 
