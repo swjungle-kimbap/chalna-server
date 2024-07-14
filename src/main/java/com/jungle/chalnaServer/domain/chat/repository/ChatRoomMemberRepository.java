0d6eea6ff7421e8f4ee8cee4e3c8a31f1d6785f5
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-07-15 00:55:16 +0900

    feat: 친구 요청 수락/거절 구현 (#242)

--- src/main/java/com/jungle/chalnaServer/domain/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/com/jungle/chalnaServer/domain/chat/repository/ChatRoomMemberRepository.java
@@ -1,11 +1,8 @@
 package com.jungle.chalnaServer.domain.chat.repository;
 
 
-import com.jungle.chalnaServer.domain.chat.domain.entity.ChatRoom;
 import com.jungle.chalnaServer.domain.chat.domain.entity.ChatRoomMember;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,10 +11,10 @@ import java.util.Optional;
 @Repository
 public interface ChatRoomMemberRepository  extends JpaRepository<ChatRoomMember, Long> {
     List<ChatRoomMember> findByMemberId(Long memberId);
-    List<ChatRoomMember> findByChatRoomId(Long chatRoomId);
-    Optional<ChatRoomMember> findByMemberIdAndChatRoomId(Long memberId, Long chatRoomId);
-    boolean existsByMemberIdAndChatRoomId(Long memberId, Long chatRoomId);
 
-    @Query("SELECT crm1.chatRoom FROM ChatRoomMember crm1 JOIN ChatRoomMember crm2 ON crm1.chatRoom = crm2.chatRoom " +
-            "WHERE crm1.member.id = :memberId1 AND crm2.member.id = :memberId2 AND crm1.chatRoom.type <> 'LOCAL'")
-    Optional<ChatRoom> findChatRoomIdByMembers(@Param("memberId1") Long memberId1, @Param("memberId2") Long memberId2);}
+    List<ChatRoomMember> findByChatRoomId(Long chatRoomId);
+
+    Optional<ChatRoomMember> findByMemberIdAndChatRoomId(Long memberId, Long chatRoomId);
+
+    boolean existsByMemberIdAndChatRoomId(Long memberId, Long chatRoomId);
+}
